a01dba3eca87dc71e2285eb6b94060c625c3c76b
mutukurinarendra <narendracm196@gmail.com>
2026-04-02 00:00:52 +0530

    dbupdated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/photo_studio_db", {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
