14fee1b5f4d2b2f1c3ab22227152a64b6ef85ac6
mutukurinarendra <narendracm196@gmail.com>
2026-04-02 00:33:22 +0530

    hii

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://frontend-5o33.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
